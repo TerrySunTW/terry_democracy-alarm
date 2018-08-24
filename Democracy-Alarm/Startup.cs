876f2ee2927ffca970f1935821ca5f6037374fed
Terry <terry@example.com>
2018-08-24 09:22:36 +0800

    move mdf to sdf

--- Democracy-Alarm/Startup.cs
+++ Democracy-Alarm/Startup.cs
@@ -1,14 +1,8 @@
-﻿using Microsoft.Owin;
-using Owin;
-
-[assembly: OwinStartupAttribute(typeof(Democracy_Alarm.Startup))]
+﻿
 namespace Democracy_Alarm
 {
     public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
-        {
-            ConfigureAuth(app);
-        }
+        
     }
 }
